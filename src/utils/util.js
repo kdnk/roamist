7f3af713acc2c69bf705e655cfb4b775965393cb
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-01-15 14:45:33 +0900

    chore: improve date

--- src/utils/util.js
+++ src/utils/util.js
@@ -14,12 +14,9 @@ window.roamTodoistIntegration.settings = {
 
 
 window.roamTodoistIntegration.convertToRoamDate = (dateString) => {
-  const parsedDate = dateString.split('-');
-  const year = parsedDate[0];
-  const month = Number(parsedDate[1]);
+  const [ year, month, day ] = dateString.split('-').map((v) => Number(v));
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const monthName = months[month - 1];
-  const day = Number(parsedDate[2]);
   const suffix = (day >= 4 && day <= 20) || (day >= 24 && day <= 30)
     ? "th"
     : ["st", "nd", "rd"][day % 10 - 1];
