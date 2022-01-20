ab12b08fde6c3bb273e74007c9a176226ce696cc
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-01-20 20:14:38 +0900

    chore: improve index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const ID = "roam-todoist-integration";
 
-// const existing = document.getElement.getElementById(ID);
+// const existing = document.getElementById(ID);
 // if (!existing) {
 //   const script = document.createElement("script");
 //   script.src = "kdnk.github.io/roam-todoist-integration/src/index.js";
@@ -19,11 +19,14 @@ const features = [
 ];
 
 features.forEach((feature) => {
-  const url = `kdnk.github.io/roam-todoist-integration/src/${feature}.js`;
-  const script = document.createElement("script");
-  script.src = `kdnk.github.io/roam-todoist-integration/src/${feature}.js`;
-  script.id = `${ID}-${feature}`;
-  script.async = true;
-  script.type = "text/javascript";
-  document.getElementsByTagName("head")[0].appendChild(script);
+  const FEATURE_ID = `${ID}-${feature}`;
+  const existing = document.getElementById(FEATURE_ID);
+  if (!existing) {
+    const script = document.createElement("script");
+    script.src = `kdnk.github.io/roam-todoist-integration/src/${feature}.js`;
+    script.id = `${ID}-${feature}`;
+    script.async = true;
+    script.type = "text/javascript";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
 });
