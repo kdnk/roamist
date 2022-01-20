aca0fb446ac0f9653bd43ec6906419bd0df22183
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-01-20 21:02:48 +0900

    chore: add reference

--- src/utils/util.js
+++ src/utils/util.js
@@ -3,6 +3,7 @@
 window.RTI = {};
 window.RTI.TODOIST_TAG_NAME = "42Todoist";
 
+ref. https://github.com/dvargas92495/SmartBlocks/issues/187#issuecomment-766252353
 window.RTI.convertToRoamDate = (dateString) => {
   const [year, month, day] = dateString.split("-").map((v) => Number(v));
   const months = [
