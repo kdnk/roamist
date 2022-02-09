b142bd68f3c227922d84dabeafdf4d5f929016fc
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-02-09 21:52:32 +0900

    setup(typescript): add types

---
+++ src/global.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-var */
+declare var roam42: any;
+declare var RTI: any;
+declare var TODOIST_TOKEN: any;
