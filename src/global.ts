1a7cc515fc27d0b2659e2d80391e5c410bba3b9b
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-02-10 01:07:00 +0900

    feat: make roamist work with old setup

--- src/global.ts
+++ src/global.ts
@@ -3,5 +3,9 @@
 /* eslint-disable no-var */
 declare var roam42: any;
 declare var Roamist: any;
+/**
+  * @deprecated
+  */
+declare var RTI: any;
 declare var TODOIST_TOKEN: any;
 declare var roamAlphaAPI: any;
--- src/js/index.ts
+++ src/js/index.ts
@@ -3,11 +3,18 @@ import { pullTasks } from "./pull-tasks";
 import { syncCompleted } from "./sync-completed";
 
 window.Roamist = {
+  ...window.RTI,
   ...window.Roamist,
   completeTask,
   pullTasks,
   syncCompleted,
 };
+
+window.RTI = {
+  completeTask,
+  pullTasks,
+  syncCompleted,
+}
 
 console.log("<<< roamist >>> window.Roamist: ", window.Roamist);
 console.log("<<< roamist >>> setup compoleted.");
