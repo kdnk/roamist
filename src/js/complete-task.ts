9925d905dbf068477f1eb98550337c15d54bbdbf
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-02-10 00:28:00 +0900

    feat: use TodoistApi for complete-task

--- src/js/complete-task.ts
+++ src/js/complete-task.ts
@@ -1,23 +1,26 @@
-export const completeTask = async () => {
-  const TODOIST_TOKEN = window.TODOIST_TOKEN;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TodoistApi } from "@doist/todoist-api-typescript";
 
+const api = new TodoistApi(window.TODOIST_TOKEN);
+
+const logger = (log: any) => {
+  console.log(`<<<<<<<<< [roamist] complete-task >>>>>>>>>: ${log}`);
+};
+
+export const completeTask = async () => {
   const blockUid = roam42.common.currentActiveBlockUID();
   const blockInfo = await roam42.common.getBlockInfoByUID(blockUid);
   const block = blockInfo[0][0];
   const text = block?.string;
-  const res = text.match(/\d{10}/);
-  const url = "https://api.todoist.com/rest/v1/tasks/" + res + "/close";
+  const todoistId = text.match(/\d{10}/);
 
-  const bearer = "Bearer " + TODOIST_TOKEN;
-  await fetch(url, {
-    method: "POST",
-    headers: {
-      Authorization: bearer,
-    },
-  });
-
-  const newContent = block.string.replace("{{[[TODO]]}}", "{{[[DONE]]}}");
-  await roam42.common.updateBlock(blockUid, newContent);
-
-  return "";
+  try {
+    await api.closeTask(todoistId);
+    const newContent = block.string.replace("{{[[TODO]]}}", "{{[[DONE]]}}");
+    await roam42.common.updateBlock(blockUid, newContent);
+    logger("succeeded.");
+  } catch (e) {
+    logger("failed.");
+    logger(e);
+  }
 };
