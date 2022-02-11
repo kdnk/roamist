bd57b4072ff51b59d98a466df3523eb609b4673e
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-02-11 14:01:02 +0900

    chore: improve getTodoBlocksWithTodoistId

--- src/js/features/sync-completed/get-todo-blocks-with-todoist-id.ts
+++ src/js/features/sync-completed/get-todo-blocks-with-todoist-id.ts
@@ -1,23 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const getTodoBlocksReferringToCurrentPage = async (title: string) => {
+// eslint-disable @typescript-eslint/no-explicit-any
+export const getTodoBlocksReferringToRoamist = async () => {
+  const title = window.Roamist.TODOIST_TAG_NAME;
   try {
-    return await window.roamAlphaAPI.q(`
+    const blocks = await window.roamAlphaAPI.q(`
         [:find (pull ?refs [:block/string :block/uid {:block/children ...}])
           :where [?refs :block/refs ?title][?refs :block/refs ?todoTitle][?todoTitle :node/title "TODO"][?title :node/title "${title}"]]`);
+    return blocks as { string: string; uid: string }[][];
   } catch (e) {
-    console.log("error in getTodoBlocksReferringToCurrentPage: ", e);
+    console.log("error in getTodoBlocksReferringToRoamist: ", e);
     return [];
   }
 };
 
 export const getTodoBlocksWithTodoistId = async () => {
-  const roamTodoBlocks = await getTodoBlocksReferringToCurrentPage(
-    window.Roamist.TODOIST_TAG_NAME
-  );
-  return roamTodoBlocks.map((item: any) => {
+  const roamTodoBlocks = await getTodoBlocksReferringToRoamist();
+  return roamTodoBlocks.map((item) => {
     const block = item[0];
     const { string } = block;
-    const todoistId = string.match(/\d{10}/)[0];
+    const matched = string.match(/\d{10}/);
+    if (!matched) {
+      throw "no match (getTodoBlocksWithTodoistId).";
+    }
+    const todoistId = matched[0];
+
     return {
       ...block,
       todoistId,
