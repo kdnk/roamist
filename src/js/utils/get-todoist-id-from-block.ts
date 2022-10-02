c5ff85ffc1397b47e4b3346de04fea129bf8d1f9
Kodai Nakamura <kodai.nakamura.215@gmail.com>
2022-10-02 17:33:42 +0900

    chore: remove unused logger

--- src/js/utils/get-todoist-id-from-block.ts
+++ src/js/utils/get-todoist-id-from-block.ts
@@ -1,12 +1,7 @@
-import { createLogger } from "./create-loagger";
-
-const logger = createLogger("get-todoist-id-from-block");
 export const getTodoistIdFromBlock = (text: string) => {
   try {
     const matched = text.match(/Todoist\/\d{10}/);
-    logger(`matched: ${matched}`);
     if (!matched) {
-      logger(`TodoistId is not found.`);
       return "";
     }
     const todoistId = matched[0]?.replace("Todoist/", "");
